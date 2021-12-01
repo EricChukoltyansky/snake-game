bc284fa8400afdf75cf24e9e4b6a12696fe9cf22
EricChukoltyansky <kazohelmet@gmail.com>
2021-12-01 13:32:14 +0200

    first commit

---
+++ game.js
@@ -0,0 +1,56 @@
+import {
+  update as updateSnake,
+  draw as drawSnake,
+  SNAKE_SPEED,
+  getSnakeHead,
+  snakeIntersection,
+} from "./snake.js";
+import { update as updateFood, draw as drawFood } from "./food.js";
+import { outsideGrid } from "./grid.js";
+
+let lastRenderTime = 0;
+let gameOver = false;
+const gameBoard = document.getElementById("game-board");
+
+function main(currentTime) {
+  if (gameOver) {
+    return alert("You Lost!");
+  }
+
+  window.requestAnimationFrame(main);
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return;
+
+  lastRenderTime = currentTime;
+
+  update();
+  draw();
+}
+
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+  updateFood();
+  checkDeath();
+}
+function draw() {
+  gameBoard.innerHTML = "";
+  drawSnake(gameBoard);
+  drawFood(gameBoard);
+}
+
+function checkDeath() {
+  gameOver = outsideGrid(getSnakeHead()) || snakeIntersection();
+}
+
+
+
+
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/EricChukoltyansky/snake-game.git
+// git push -u origin main
